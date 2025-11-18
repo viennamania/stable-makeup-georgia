95eb14b27cc655485a461ee67660b10336703601
seunghyonpark <creath.park@gmail.com>
2025-11-18 10:47:52 +0900

    Refactor: Simplify store filtering logic in CenterConsole component

--- src/components/CenterConsole.tsx
+++ src/components/CenterConsole.tsx
@@ -365,27 +365,8 @@ const CenterConsole = () => {
             // set list if viewOnAndOff is true
             const filteredStores = data.result.stores.filter((store: { viewOnAndOff: boolean }) => store.viewOnAndOff === true);
 
+            setStores(filteredStores || []);
 
-            //alert("filteredStores.length: " + filteredStores.length);
-
-
-            //console.log("filteredStores", filteredStores);
-
-            /*
-            //setStores(data.result.stores || []);
-            // if count is more than 10, slice to 10
-            if (data.result.stores.length > 10) {
-              setStores(data.result.stores.slice(0, 10) || []);
-            } else {
-              setStores(data.result.stores || []);
-            }
-            */
-
-            if (filteredStores.length > 10) {
-              setStores(filteredStores.slice(0, 10) || []);
-            } else {
-              setStores(filteredStores || []);
-            }
 
             setTotalCurrentUsdtBalance(data.result?.totalCurrentUsdtBalance || 0);
           } else {
@@ -405,45 +386,7 @@ const CenterConsole = () => {
 
   }, [address, isAdmin]);
 
-  /*
-    {
-      totalCount: 5,
-      stores: [
-        {
-          _id: new ObjectId('68ad0cced375320e8a69b2ea'),
-          storecode: 'krbdscsd',
-          storeName: 'confection',
-          storeLogo: 'https://t0gqytzvlsa2lapo.public.blob.vercel-storage.com/P7DIjS7-DxG2zcp7o3qGKniSLTi1UDFehe0akM.png',
-          createdAt: '2025-08-26T01:24:30.959Z',
-          backgroundColor: 'yellow-100',
-          settlementWalletAddress: '0x4429A977379fdd42b54A543E91Da81Abe7bb52FD',
-          totalUsdtAmount: 118.19,
-          currentUsdtBalance: 2098.3850755020003,
-          liveOnAndOff: true
-        },
-        {
-          _id: new ObjectId('68ad00d15359024833432764'),
-          storecode: 'jysmbsco',
-          storeName: 'macaron',
-          storeLogo: 'https://t0gqytzvlsa2lapo.public.blob.vercel-storage.com/IYigWCF-vj1meScA5QItw3RRVaqxCkEWI98Ay1.png',
-          createdAt: '2025-08-26T00:33:21.613Z',
-          backgroundColor: 'blue-100',
-          settlementWalletAddress: '0x4429A977379fdd42b54A543E91Da81Abe7bb52FD',
-          totalUsdtAmount: 93.96,
-          currentUsdtBalance: 2098.3850755020003,
-          liveOnAndOff: false
-        },
-      ],
-      totalCurrentUsdtBalance: 4196.7701510040006
-    }
-  */
 
-
-  // liveOnAndOff
-  // /api/store/toggleLive
-  // toggling array of objects in state
-  // find the object in the array and update it
-  // const updatedStores = stores.map(store => {
   const [isToggling, setIsToggling] = useState([] as Array<string>);
 
 
@@ -485,12 +428,7 @@ const CenterConsole = () => {
 
   };
 
-
-
-
- const [showStores, setShowStores] = useState(false);
-
-
+  const [showStores, setShowStores] = useState(false);
 
   return (
     <>
