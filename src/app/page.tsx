e9636891bc97fce31b0ba71eb3363afbe295a622
seunghyonpark <creath.park@gmail.com>
2026-05-27 16:57:19 +0900

    Redirect root to English promotion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { DEFAULT_LOCALE, LOCALE_COOKIE_NAME, resolveLocale } from "@/lib/i18n";
-
 export default function Page() {
-  const preferredLocale = resolveLocale(
-    cookies().get(LOCALE_COOKIE_NAME)?.value,
-    DEFAULT_LOCALE,
-  );
-
-  redirect(`/${preferredLocale}/homepage`);
+  redirect("/en/promotion");
 }
